ce1854b04005bbdf73fee65917b870130f21fb9d
Aashish_kumar2 <ritikkamboj4314@gmail.com>
2025-07-19 18:52:00 +0530

    applied animation on toast

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,22 @@ import './App.css'
 function App() {
 
   return (
-   <div>
-    <h1>React Toaster App</h1>
+    <div className='container'>
+      <div className='toast-container'>
+        <div className='toast'>
+          Success Toast <span>X</span>
+        </div>
+      </div>
+      <h1>React Toaster App</h1>
+      <div className='btn-container'>
+        <button>Success Toast</button>
+        <button>Info Toast</button>
+        <button>Warning Toast</button>
+        <button>Error Toast</button>
 
-   </div>
+      </div>
+
+    </div>
   )
 }
 
