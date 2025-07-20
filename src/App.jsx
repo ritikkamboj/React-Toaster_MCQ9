f0fc1ee2f07e4b6be7c5e26241c208c8b405fcd9
Aashish_kumar2 <ritikkamboj4314@gmail.com>
2025-07-20 19:00:27 +0530

    created toast from list

--- src/App.jsx
+++ src/App.jsx
@@ -2,34 +2,45 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [visible , setVisible] = useState(false);
+  // const [visible , setVisible] = useState(false);
+  const [list, setList] = useState([]);
 
-  function handleClick()
-  {
-    setVisible(true)
-    setTimeout(()=> handleClose() , 5000)
+  function handleClick(message, type) {
+    const id = new Date().getTime();
+    const newToast = [...list, { id, message, type }]
+    setList(newToast);
+
+    // setVisible(true)
+    // setTimeout(()=> handleClose() , 5000)
+
+
   }
 
-  function handleClose()
-  {
+  function handleClose() {
     setVisible(false)
   }
 
   return (
     <div className='container'>
-     {
-      visible &&  <div className='toast-container'>
-        <div className='toast'>
-          Success Toast <span onClick={() => handleClose()}>X</span>
+
+      <div className='toast-container'>
+        {
+          list.map((data)=> {
+            return    <div key={data.id} className={`toast ${data.type}`}>
+        {data.message} <span onClick={() => handleClose("Success", 'success')}>X</span>
         </div>
+          })
+        }
+     
       </div>
-     }
+
+
       <h1>React Toaster App</h1>
       <div className='btn-container'>
-        <button onClick={() => handleClick()}>Success Toast</button>
-        <button>Info Toast</button>
-        <button>Warning Toast</button>
-        <button>Error Toast</button>
+        <button onClick={() => handleClick("Success" , 'success')}>Success Toast</button>
+        <button onClick={() => handleClick("Info" , 'info')}>Info Toast</button>
+        <button onClick={() => handleClick("Warning" , 'warning')}>Warning Toast</button>
+        <button onClick={() => handleClick("Error" , 'error')}>Error Toast</button>
 
       </div>
 
